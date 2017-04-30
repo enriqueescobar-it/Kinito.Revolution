07ab9470d378e2ed48e5e814355f021f68ca63f6
enriqueescobar-askida <enrique.escobar@askida.com>
2017-04-30 18:01:47 -0400

    -Dane- reading and copying files to work with

--- Dane.Export.Indicators/Dane.Export.Indicators.R
+++ Dane.Export.Indicators/Dane.Export.Indicators.R
@@ -7,11 +7,27 @@ lastDir <- tail(dirList, n = 1);
 rm(dirList);
 
 fileList <- GetFullnameFiles(lastDir);
+newFileList <- fileList;
 rm(lastDir);
 
 for (i in seq(along = fileList)) {
-  cat(paste0(i, "\t", fileList[i], "\n"));
-  if(!file.exists(fileList[i])) cat("aca");
-  #library(data.table);
-  #df <- read.table(file=fileList[i], header=TRUE);
+  oldFile <- fileList[i];
+  cat(paste0("old\t", oldFile, "\n"));
+  newFile <- RmAccent(oldFile);
+  cat(paste0("new\t", newFile, "\n"));
+  newFileList[i] <- newFile;
+  if(!file.exists(oldFile)) {
+    cat("ERROR|FilenameFormatError: Cannot read file\n");
+  } else {
+     #CopyFullnameFiles(oldFilePath = oldFile, newFilePath = newFile);
+  }
+  rm(oldFile);
+  rm(newFile);
 }
+rm(fileList);
+rm(i);
+
+df <- DaneTabbedFileToDF(aFilePath = newFileList[1]);
+View(df);
+
+
--- Dane.Export.Indicators/Lib/Dane.Export.Indicators.Util.R
+++ Dane.Export.Indicators/Lib/Dane.Export.Indicators.Util.R
@@ -27,9 +27,17 @@ GetFullnameDirectories <- function(aDir = ""){
   
   ArgumentCheck::finishArgCheck(argumentCheck);
   
-  return(list.dirs(path = aDir, full.names = TRUE, recursive = FALSE));
+  return(base::list.dirs(path = aDir, full.names = TRUE, recursive = FALSE));
 }
 
+#' Title GetFullnameFiles
+#'
+#' @param aDir 
+#'
+#' @return full path file list
+#' @export TBD
+#'
+#' @examples TBD
 GetFullnameFiles <- function(aDir = ""){
   
   argumentCheck <- ArgumentCheck::newArgCheck();
@@ -43,5 +51,51 @@ GetFullnameFiles <- function(aDir = ""){
   
   ArgumentCheck::finishArgCheck(argumentCheck);
   
-  return(list.files(aDir, all.files = TRUE, full.names = TRUE)[-c(1, 2)]);
+  return(base::list.files(aDir, all.files = TRUE, full.names = TRUE)[-c(1, 2)]);
+}
+
+CopyFullnameFiles <- function(oldFilePath = "", newFilePath = "") {
+  
+  base::file.copy(oldFilePath, newFilePath, overwrite = TRUE, recursive = FALSE,
+                  copy.mode = TRUE, copy.date = TRUE);
+}
+
+RmAccent <- function(aString = "") {
+  
+  replaceCharList <- list('Š'='S', 'š'='s', 'Ž'='Z', 'ž'='z', 'Ç'='C', 'ç'='c',
+                           'Ñ'='N', 'ñ'='n', 'Þ'='B', 'þ'='b',
+                           'ß'='Ss', '╡'='A', '¦'='A', '('='', ')'='',
+                           'À'='A', 'Á'='A', 'Â'='A', 'Ä'='A', 'Ã'='A', 'Å'='A', 'Æ'='A',
+                           'à'='a', 'á'='a', 'â'='a', 'ä'='a', 'ã'='a', 'å'='a', 'æ'='a',
+                           'È'='E', 'É'='E', 'Ê'='E', 'Ë'='E',
+                           'è'='e', 'é'='e', 'ê'='e', 'ë'='e',
+                           'Ì'='I', 'Í'='I', 'Î'='I', 'Ï'='I',
+                           'ì'='i', 'í'='i', 'î'='i', 'ï'='i',
+                           'Ù'='U', 'Ú'='U', 'Û'='U', 'Ü'='U',
+                           'ù'='u', 'ú'='u', 'û'='u', 'ü'='u',
+                           #'Ỳ'='Y',
+                           'Ý'='Y', 'Ŷ'='Y', 'Ÿ'='Y',
+                           #'ỳ'='y',
+                           'ý'='y', 'ŷ'='y', 'ÿ'='y',
+                           'Ò'='O', 'Ó'='O', 'Ô'='O', 'Ö'='O', 'Õ'='O', 'Ø'='O',
+                           'ò'='o', 'ó'='o', 'ô'='o', 'õ'='o', 'ö'='o', 'ø'='o', 'ð'='o');
+  aString <- gsub("['`^~\"]", " ", aString);
+  aString <- iconv(aString, to = "ASCII//TRANSLIT//IGNORE");
+  aString <- gsub("`|\\'", "", aString);
+  aString <- iconv(aString, to = "ASCII//TRANSLIT//IGNORE");
+  aString <- gsub(pattern = " \\(", replacement = "_", x = aString);
+  aString <- gsub(pattern = "\\)", replacement = "_", x = aString);
+  aString <- gsub(pattern = "\\|", replacement = "A", x = aString);
+  aString <- gsub(pattern = " -", replacement = "-", x = aString);
+  aString <- gsub(pattern = "- ", replacement = "-", x = aString);
+  aString <- gsub(pattern = " ", replacement = "_", x = aString);
+  
+  return(aString);
+}
+
+library(readr);
+
+DaneTabbedFileToDF <- function(aFilePath = "") {
+  
+  return(readr::read_delim(aFilePath, "\t", escape_double = FALSE, na = "NA", trim_ws = TRUE));
 }
