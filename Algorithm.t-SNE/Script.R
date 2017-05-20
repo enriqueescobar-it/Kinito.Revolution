bf9d36c5af590ab996a9f74760228326c3789d28
enriqueescobar-askida <enrique.escobar@askida.com>
2017-05-19 21:09:24 -0400

    -update-

--- Algorithm.t-SNE/Script.R
+++ Algorithm.t-SNE/Script.R
@@ -1,17 +1,21 @@
 ## calling the installed package
-train <- read.csv(file.choose()) ## Choose the train.csv file downloaded from the link above  
-library(Rtsne)
+library(Rtsne);
+## Choose the train.csv file downloaded from the link above "E:/Disk_X/Kinito.Revolution/Algorithm.t-SNE/Data/train.csv"
+train <- read.csv(file.choose());
 ## Curating the database for analysis with both t-SNE and PCA
-Labels <- train$label
-train$label <- as.factor(train$label)
+labelList <- train$label;
+train$label <- as.factor(train$label);
 ## for plotting
-colors = rainbow(length(unique(train$label)))
-names(colors) = unique(train$label)
-
+colorList = rainbow(length(unique(train$label)));
+names(colorList) = unique(train$label);
 ## Executing the algorithm on curated data
-tsne <- Rtsne(train[, -1], dims = 2, perplexity = 30, verbose = TRUE, max_iter = 500)
-exeTimeTsne <- system.time(Rtsne(train[, -1], dims = 2, perplexity = 30, verbose = TRUE, max_iter = 500))
-
+tsne <- Rtsne::Rtsne(train[, -1], dims = 2, perplexity = 30, verbose = TRUE,
+                     max_iter = 500);
+exeTimeTsne <- system.time(
+        Rtsne::Rtsne(train[, -1], dims = 2, perplexity = 30, verbose = TRUE,
+                     max_iter = 500));
 ## Plotting
-plot(tsne$Y, t = 'n', main = "tsne")
-text(tsne$Y, labels = train$label, col = colors[train$label])
+tsneMainTitle <- "Algorithm t-SNE";
+plot(tsne$Y, t = 'n', main = tsneMainTitle);
+text(tsne$Y, labels = train$label, col = colorList[train$label]);
+
